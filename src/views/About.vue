c274aee2a6913d60d491e938960831cadd021683
lixiangjie <773983534@qq.com>
2021-02-07 17:39:29 +0800

    第一次提交

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button type="warning" plain>警告按钮</el-button>
+    <el-button type="danger" round>危险按钮</el-button>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,14 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <div>
+    <p>
+      If Element is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button>el-button</el-button>
+  </div>
 </template>
 
 <script>
